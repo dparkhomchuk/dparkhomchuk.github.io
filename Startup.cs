cc970d298e3dddb251dc88b7ac6596d0e13301b2
dparkhomchuk <sailorparhom@bigmir.net>
2017-04-30 11:36:54 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthorizationPrototype.Startup))]
+namespace AuthorizationPrototype
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
