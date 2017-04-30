d431b5f53fad59a02d5820bd9fa88ab4dbd83081
dparkhomchuk <sailorparhom@bigmir.net>
2017-04-30 11:28:56 +0300

    second

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using AuthorizationPrototype.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebPrototype.Models;
+
+namespace AuthorizationPrototype.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Location> _location = new List<Location>();
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult PostPoint(StationDTO dto)
+        {
+            var station = new Location();
+            station.GeoLat = dto.GeoLat;
+            station.GeoLong = dto.GeoLong;
+            station.PlaceCategory = dto.PlaceCategory;
+            station.PlaceName = dto.PlaceName;
+            station.ToiletAvail = dto.ToiletAvail;
+            station.TrafficLightAvail = dto.TrafficLightAvail;
+            station.MenuAvail = dto.MenuAvail;
+            station.VisualAvail = dto.VisualAvail;
+            var random = new Random((int)DateTime.Now.Ticks);
+            var value = random.Next(0, 3);
+            station.AvailabilityType = (AvailabilityType)random.Next(0,3);
+            _dbContext.Locations.Add(station);
+            _dbContext.SaveChanges();
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+        public JsonResult GetData()
+        {
+            _location = _dbContext.Locations.ToList();
+            return Json(_location, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ DTO/StationDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPrototype.Models
+{
+    public class StationDTO
+    {
+        public string PlaceName { get; set; } // название места
+        public string PlaceCategory { get; set; }
+        public double GeoLong { get; set; } // долгота - для карт google
+        public double GeoLat { get; set; } // широта - для карт google
+        public AvailabilityType ToiletAvail { get; set; }
+        public AvailabilityType TrafficLightAvail { get; set; }
+        public AvailabilityType MenuAvail { get; set; }
+        public AvailabilityType VisualAvail { get; set; }
+    }
+}
